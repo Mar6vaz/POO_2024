a685818a65fd1b30d11f055bb0ff13c0a0249768
Mar06vaz <marielavazquez24896@gmail.com>
2024-05-15 20:47:57 -0600

    first commit

---
+++ 2_variables_tipo_datos/variables.py
@@ -0,0 +1,25 @@
+#Las variables son debilmente tipadas
+#es decir no es necesario indicar el tipo de
+#dato al momento de utilizarlas
+
+#Variables
+edad=30
+estatura=1.80
+email="marielavazquez24896@gmail.com"
+logica=True
+caracter='@'
+
+#Imprimir contenido de las variables
+print(edad)
+print(estatura)
+print(email)
+print(logica)
+print(caracter)
+
+#conocer el tipo de dato que python asigno
+print(type(edad))
+print(type(estatura))
+print(type(email))
+print(type(email))
+print(type(logica))
+print(type(caracter))
